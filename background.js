2be78344950b4d76f48be15a27a2915d1817daca
Sachin <sachnaror@users.noreply.github.com>
2024-07-19 19:28:41 +0530

    Delete background.js

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url) {
-    chrome.tabs.executeScript(tabId, { file: "content.js" });
-  }
-});
